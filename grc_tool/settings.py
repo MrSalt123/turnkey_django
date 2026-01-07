c8c57eef852109ff0ba04308081a0e04e95a6b41
MrSalt123 <119909394+MrSalt123@users.noreply.github.com>
2026-01-07 11:02:46 -0500

    settings

--- grc_tool/settings.py
+++ grc_tool/settings.py
@@ -75,6 +75,7 @@ WSGI_APPLICATION = "grc_tool.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
+
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
            'NAME': os.getenv('DATABASE_NAME'),
